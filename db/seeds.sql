139fd511c0ff0baafce1a8556782e3a471fe650c
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-03 21:57:31 -0400

    commit

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Classic Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Sriracha Burger", false);
