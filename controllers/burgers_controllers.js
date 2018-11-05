522d6c988dd4e14f9c8cffabafcbcc092ac9eebc
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-04 20:43:10 -0500

    Commit

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,2 +1,17 @@
 var express = require("express");
 
+var burger = require("models/burger.js");
+
+var router = express.Router();
+
+router.post("/api/cats", function(req, res) {
+
+});
+
+router.put("/api/cats/:id", function(req, res) {
+
+});
+
+router.delete("/api/cats/:id", function(req, res) {
+
+});
