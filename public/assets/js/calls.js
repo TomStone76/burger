aad7cceafaa93650a543e218d1425f4eabd2db4e
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-07 18:29:54 -0500

    commit

--- public/assets/js/calls.js
+++ public/assets/js/calls.js
@@ -1,18 +1,36 @@
-$(function() {
-    $(".add-button").on("click", function(event) {
-        var id = $(this).data("id");
-        var newState = $(this.data("newstatus");
+$(function () {
+            $(".add-button").on("click", function (event) {
+                    var id = $(this).data("id");
+                    var newState = $(this.data("newstatus");
 
-        var newDevouredState = {
-            devoured: newState
-        };
+                        var newDevouredState = {
+                            devoured: newState
+                        };
 
-        $.ajax("/api/cats/" + id, {
-            type: "PUT",
-            data: newDevouredState
-        }).then(
-            function() {
-                console.log("changed status to " + newState);
-                location    
+                        $.ajax("/api/cats/" + id, {
+                            type: "PUT",
+                            data: newDevouredState
+                        }).then(
+                            function () {
+                                console.log("changed status to " + newState);
+                                location.reload();
+                            }
+                        );
+                    }); $("create-form").on("submit", function (event) {
+                    event.preventDefault()
 
-});
+                    var newBurger = {
+                        name: $("#bu").val().trim()
+                    };
+
+                    $.ajax("/api/burgers", {
+                        type: "POST",
+                        data: newBurger
+                    }).then(
+                        function () {
+                            console.log("created new burger");
+                            location.reload();
+                        }
+                    );
+                });
+            });
