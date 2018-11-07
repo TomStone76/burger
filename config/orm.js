ab2e76113ef3c4fdb1b8a2751ae3fe3350d1d115
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-07 16:09:36 -0500

    commit

--- config/orm.js
+++ config/orm.js
@@ -12,11 +12,25 @@ var orm = {
             cb(result);
         });
     },
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
 
-    // function insertOne() {
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
 
-    // }
+        console.log(queryString);
 
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
     update: function (table, objColVals, condition, cb) {
         var queryString = "UPDATE " + table;
         queryString += " SET ";
