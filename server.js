8b19c46c386690d8ba681ad42e971bc3ac9ceb5c
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-07 14:32:26 -0500

    Commit

--- server.js
+++ server.js
@@ -6,15 +6,22 @@ var app = express();
 
 app.use(express.statis("public"));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controllers");
 
 app.use(routes);
 
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
