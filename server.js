d2bbd94d3afe1d4a68e3fc58acd62fc9768e297c
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-07 14:28:48 -0500

    commit

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 let express = require("express");
+
+var PORT = process.env.PORT || 8000;
+
+var app = express();
+
+app.use(express.statis("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
