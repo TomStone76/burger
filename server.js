e24df6248674e93a0173f3315d0b27e11681f209
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-07 14:33:06 -0500

    commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var PORT = process.env.PORT || 8000;
 
 var app = express();
 
-app.use(express.statis("public"));
+app.use(express.static("public"));
 
 app.use(express.urlencoded({
     extended: true
