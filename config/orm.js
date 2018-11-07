6889ae34e040d9e52ed733161dff42f950675629
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-07 14:37:59 -0500

    commit

--- config/orm.js
+++ config/orm.js
@@ -3,34 +3,35 @@ var connection = require("connection.js");
 
 var orm = {
 
-all: function (tableInput, cb) {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, results) {
-        if (err) {
-            throw err;
-        }
-        cb(result);
-    });
-},
+    all: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, results) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
 
-function insertOne() {
+    // function insertOne() {
 
-}
+    // }
 
-update: function(table, objColVals, condition cb) {
-    var queryString = "UPDATE " + table;
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
+    update: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
 
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
-        if (err) {
-            throw err;
-        }
-        cb(result);
-    });
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
 }
 
 module.exports = orm;
--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,6 +1,6 @@
 var express = require("express");
 
-var burger = require("models/burger.js");
+var burger = require("../models/burger");
 
 var router = express.Router();
 
