5cdfe1ce8402d2a0ef0b8c31254ed9dc5913d9db
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-09 13:42:16 -0500

    commit

--- config/orm.js
+++ config/orm.js
@@ -1,51 +1,66 @@
 var connection = require("../config/connection.js");
 
-//More or less use exact code from cat app
-var orm = {
+function printQuestionMarks(num) {
+    var arr = [];
 
-    all: function (tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
-        });
-    },
-    insertOne: function (table, cols, vals, cb) {
-        var queryString = "INSERT INTO " + table;
-
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
-
-        console.log(queryString);
-
-        connection.query(queryString, vals, function (err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
-        });
-    },
-    update: function (table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
-
-        console.log(queryString);
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
-        });
+    for (var i = 0; i < num; i++) {
+        arr.push("?")
     }
+    return arr.toString();
 }
 
-module.exports = orm;
+function objToSql(ob) {
+    
+}
+
+
+//More or less use exact code from cat app
+// var orm = {
+
+//     all: function (tableInput, cb) {
+//         var queryString = "SELECT * FROM " + tableInput + ";";
+//         connection.query(queryString, function (err, result) {
+//             if (err) {
+//                 throw err;
+//             }
+//             cb(result);
+//         });
+//     },
+//     insertOne: function (table, cols, vals, cb) {
+//         var queryString = "INSERT INTO " + table;
+
+//         queryString += " (";
+//         queryString += cols.toString();
+//         queryString += ") ";
+//         queryString += "VALUES (";
+//         queryString += printQuestionMarks(vals.length);
+//         queryString += ") ";
+
+//         console.log(queryString);
+
+//         connection.query(queryString, vals, function (err, result) {
+//             if (err) {
+//                 throw err;
+//             }
+//             cb(result);
+//         });
+//     },
+//     update: function (table, objColVals, condition, cb) {
+//         var queryString = "UPDATE " + table;
+//         queryString += " SET ";
+//         queryString += objToSql(objColVals);
+//         queryString += " WHERE ";
+//         queryString += condition;
+
+//         console.log(queryString);
+//         connection.query(queryString, function (err, result) {
+//             if (err) {
+//                 throw err;
+//             }
+//             cb(result);
+//         });
+//     }
+// }
+
+// module.exports = orm;
+
